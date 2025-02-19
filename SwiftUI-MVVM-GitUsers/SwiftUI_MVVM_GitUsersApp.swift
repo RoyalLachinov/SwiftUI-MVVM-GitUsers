896e0bc1f03755d2c9ffa19743d5d5251144567d
RoyalLachinov <royal.lachinov@gmail.com>
2025-02-18 22:33:13 -0500

    Initial Commit

---
+++ SwiftUI-MVVM-GitUsers/SwiftUI_MVVM_GitUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVM_GitUsersApp.swift
+//  SwiftUI-MVVM-GitUsers
+//
+//  Created by Royal Lachinov on 2025-02-18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVM_GitUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
